071f280ae577e00b9722599440f5bdba7cd77d27
shaharyaruddin <sheheryark505@gmail.com>
2025-08-05 18:20:19 +0500

    optimize code

--- src/components/app-sidebar.jsx
+++ src/components/app-sidebar.jsx
@@ -12,133 +12,72 @@ import {
 import { Book, Gauge, SquareKanban, Tag, Users } from "lucide-react";
 import Cookies from "js-cookie";
 
-
 export function AppSidebar({ ...props }) {
-  const [user,setUser] = React.useState({});
+  const [user, setUser] = React.useState({});
   // This is sample data.
-  React.useEffect(()=>{
-      const cookieUser = Cookies.get('userInfo')
-      setUser(JSON.parse(cookieUser))
-  },[])
-const data = {
-  user: {
-    name: user.username ,
-    email: user.email,
-    // name: 'coder' ,
-    // email: 'coder',
-    avatar: "/avatars/shadcn.jpg",
-  },
-  navMain: [
-    {
-      title:'Dashboard',
-      url:'/',
-      icon: Gauge 
+  React.useEffect(() => {
+    const cookieUser = Cookies.get("userInfo");
+    setUser(JSON.parse(cookieUser));
+  }, []);
+  const data = {
+    user: {
+      name: user.username,
+      email: user.email,
+      // name: 'coder' ,
+      // email: 'coder',
+      avatar: "/avatars/shadcn.jpg",
     },
-    {
-      title: "Users",
-      url: "#",
-      icon: Users,
-      isActive: false,
-      items: [
-        {
-          title: "Users",
-          url: "/users",
-        },
-        {
-          title: "Block Users",
-          url: "/users/block",
-        },
-        {
-          title: "Add User",
-          url: "/users/add",
-        },
-        
-      ],
-    },
-    {
-      title: "Tools",
-      url: "#",
-      icon: Tag,
-      isActive: false,
-      items: [
-        {
-          title: "Tools",
-          url: "/tools",
-        },
-        {
-          title: "Add Tools",
-          url: "/tools/add",
-        },
-        
-        
-      ],
-    },
-    {
-      title: "Tags",
-      url: "#",
-      icon: Tag,
-      isActive: false,
-      items: [
-        {
-          title: "Tags",
-          url: "/tags",
-        },
-        {
-          title: "Add Tag",
-          url: "/tags/add",
-        },
-        
-        
-      ],
-    },
-    {
-      title: "Cateogories",
-      url: "#",
-      icon: Tag,
-      isActive: false,
-      items: [
-        {
-          title: "Cateogories",
-          url: "/categories",
-        },
-        {
-          title: "Add Cateogories",
-          url: "/categories/add",
-        }, 
-      ],
-    },
-    {
-      title: "Pricing plan",
-      url: "#",
-      icon: Tag,
-      isActive: false,
-      items: [
-        {
-          title: "plan",
-          url: "/pricing-plans",
-        },
-        {
-          title: "Add Pricing plan",
-          url: "/pricing-plans/add",
-        }, 
-      ],
-    },
-    {
-      title: "Ranking",
-      url: "/ranking",
-      icon: Tag,
-      isActive: false,
-      
-    },
-  ],
-};
+    navMain: [
+      {
+        title: "Dashboard",
+        url: "/",
+        icon: Gauge,
+      },
+
+      {
+        title: "Blogs",
+        url: "#",
+        icon: Tag,
+        isActive: false,
+        items: [
+          {
+            title: "Blogs",
+            url: "/tools",
+          },
+          {
+            title: "Add Tools",
+            url: "/tools/add",
+          },
+        ],
+      },
+
+      {
+        title: "Portfolio",
+        url: "#",
+        icon: Tag,
+        isActive: false,
+        items: [
+          {
+            title: "Cateogories",
+            url: "/categories",
+          },
+          {
+            title: "Add Cateogories",
+            url: "/categories/add",
+          },
+        ],
+      },
+    ],
+  };
 
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
         <div className="w-full flex justify-center items-center mt-3">
-         <span className="group-data-[state=collapsed]:hidden">Freemium Admin</span>
-        <span className="hidden group-data-[state=collapsed]:inline">F</span>
+          <span className="group-data-[state=collapsed]:hidden">
+            Freemium Admin
+          </span>
+          <span className="hidden group-data-[state=collapsed]:inline">F</span>
         </div>
       </SidebarHeader>
       <SidebarContent>
